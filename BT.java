3b3361cda22ce2317a5ed46e6a951abac02595ba
Milan Shingote <shingotemilan@gmail.com>
2016-06-29 11:57:43 -0700

    Add files via upload

---
+++ BT.java
@@ -0,0 +1,49 @@
+
+//Program: Check if given Binary tree is a BST or not
+ 
+class Node
+{
+    int data;
+    Node left, right;
+ 
+    public Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+ 
+public class BT
+{
+    Node root;
+    boolean isBST()  
+    {
+        return isBSTUtil(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+ 
+    boolean isBSTUtil(Node node, int min, int max)
+    {
+        if (node == null)
+            return true;
+ 
+        if (node.data < min || node.data > max)
+            return false;
+
+        return (isBSTUtil(node.left, min, node.data-1) && isBSTUtil(node.right, node.data+1, max));
+    }
+ 
+    public static void main(String args[])
+    {
+        BT tree = new BT();
+        tree.root = new Node(8);
+        tree.root.left = new Node(5);
+        tree.root.right = new Node(15);
+        tree.root.left.left = new Node(2);
+        tree.root.left.right = new Node(7);
+ 
+        if (tree.isBST())
+            System.out.println("IS BST");
+        else
+            System.out.println("Not a BST");
+    }
+}
